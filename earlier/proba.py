68d080607c4e158fd30cb36b0a0f7471583c9d32
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-15 16:17:57 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,3 +1,8 @@
-print('Dragon' + 'stone')
+euros_count = 1000
+dollars_count = euros_count * 1.25  # 1250.0
+rubles_count = dollars_count * 60   # 75000.0
+
+print(rubles_count)
+
 
 name = input('это конец')
