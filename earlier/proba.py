cfb29d8ecbb061f8f2d89fc996f2ff73e6eaa41b
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-18 23:41:20 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,8 +1,4 @@
-euros_count = 1000
-dollars_count = euros_count * 1.25  # 1250.0
-rubles_count = dollars_count * 60   # 75000.0
-
-print(rubles_count)
-
+first_name = '  Grigor   \n '
+print(first_name.strip())
 
 name = input('это конец')
