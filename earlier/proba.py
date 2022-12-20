d78814e8808b3c71ca8dc3a7c30cb1a94b4a2089
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-20 16:10:25 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,4 +1,19 @@
-first_name = '  Grigor   \n '
-print(first_name.strip())
+def trim_and_repeat(text, offset = 0, repetitions = 1):
+    result = (text[offset:] * repetitions)
+    return result
 
+print(trim_and_repeat('python'))
+
+
+# def trim_and_repeat(text, offset = 0, repetitions = 1):
+#     result = (text[offset:] * repetitions)
+#     return result
+# trim_and_repeat(text='BlackSilverUfa')
+# print(trim_and_repeat)
+
+
+# def truncate(text, length):
+#     result = f"{text[0:length]}..."
+#     return result
+# print(truncate('qwerty qwerty123', 3))
 name = input('это конец')
