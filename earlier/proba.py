758f6fdce5db1f304005f184b54938fd03de13e5
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-22 16:15:42 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,19 +1,27 @@
-def trim_and_repeat(text, offset = 0, repetitions = 1):
-    result = (text[offset:] * repetitions)
-    return result
+# import string
+#
+# def has_special_chars(str):
+#     # Проверяет содержание специальных символов в строке
+#     return any(ch for ch in str if ch in string.punctuation)
+# #
+# print(has_special_chars(''))
+#
+# # def is_strong_password(password):
+# #     length = len(password)
+# #     # Скобки задают приоритет. Понятно, что к чему относится.
+# #     return (length > 8 and length < 20) or has_special_chars(password)
+# #
+# # print(is_strong_password(''))
+# # print(is_strong_password('1'))
+# # print(is_strong_password('1234567890'))
+# # print(is_strong_password('123456789012345678901234567890'))
+# #
+# # print(is_strong_password(''))
+# # print(is_strong_password('q'))
+# # print(is_strong_password('qwertyuiop'))
+# # print(is_strong_password('qwertyuiopqwertyuiopqwertyuiop'))
 
-print(trim_and_repeat('python'))
+result = not True or False
 
 
-# def trim_and_repeat(text, offset = 0, repetitions = 1):
-#     result = (text[offset:] * repetitions)
-#     return result
-# trim_and_repeat(text='BlackSilverUfa')
-# print(trim_and_repeat)
-
-
-# def truncate(text, length):
-#     result = f"{text[0:length]}..."
-#     return result
-# print(truncate('qwerty qwerty123', 3))
-name = input('это конец')
+name = input('это конец') # это для просмотра открывающегося окна
