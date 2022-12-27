d568f93b9bc2ac4b464deb3b41013a0f18963691
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-27 16:21:05 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,13 +1,20 @@
-def string_or_not(or_not):
-    return or_not and 'yes' or 'no'
-    print(string_or_not())
+def normalize_url(url):
+    normalize = url[:7]
+    if normalize.replace('', 'https://'):
+        print(normalize.replace('', 'https://', 1))
+    elif normalize('http://'):
+        print(normalize.replace('http://', 'https://', 1))
+    else:
+        print(normalize.replace('https://', 'https://', 1))
+# s = 'hello'
+# s1 = 'http://hello'
+# s2 = 'https://hello'
+# print(s.replace('', 'https://', 1) [:]) #работает
+# print(s1.replace('http://', 'https://') [:])   #работает
+# print(s2.replace('https://', 'https://') [:]) #работает
 
-
-
-string_or_not('Hexlet')  # yes
-string_or_not(10)  # no
-string_or_not('')  # yes
-string_or_not(False)  # no
-
-
+print(normalize_url('hello'))
+print(normalize_url('http://hello'))
+print(normalize_url('https://hello'))
 name = input('это конец')
+# если в первых 7 символах есть url то оставляем url, если url нет то добавляем его
