e9c5c021ddb650d41c9441ec5484249ae27cb2c1
MyWork <92075508+zitaker@users.noreply.github.com>
2022-12-28 06:49:07 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,11 +1,16 @@
 def normalize_url(url):
     normalize = url[:7]
-    if normalize.replace('', 'https://'):
+    # if normalize.replace('', 'https://'):
+    # if normalize.replace('', 'https://') == normalize.replace('', ''):
+    if normalize.replace('', 'https://') == normalize.replace('', '333'):
         print(normalize.replace('', 'https://', 1))
-    elif normalize('http://'):
-        print(normalize.replace('http://', 'https://', 1))
-    else:
-        print(normalize.replace('https://', 'https://', 1))
+
+    elif normalize.replace('http://', 'https://'):
+        # print(normalize.replace('http://', 'https://'))
+        print(normalize.replace('http://', '111'))
+    # else:
+    #     print(normalize.replace('https://', 'https://'))
+    return "https://" + normalize
 # s = 'hello'
 # s1 = 'http://hello'
 # s2 = 'https://hello'
@@ -13,8 +18,9 @@ def normalize_url(url):
 # print(s1.replace('http://', 'https://') [:])   #работает
 # print(s2.replace('https://', 'https://') [:]) #работает
 
+
 print(normalize_url('hello'))
-print(normalize_url('http://hello'))
-print(normalize_url('https://hello'))
+print(normalize_url('http://hello1'))
+# print(normalize_url('https://hello2'))
 name = input('это конец')
 # если в первых 7 символах есть url то оставляем url, если url нет то добавляем его
