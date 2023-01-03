4f47f9610295b91c950f074037e96ca9854caced
MyWork <92075508+zitaker@users.noreply.github.com>
2023-01-03 16:15:49 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,76 +1,23 @@
-# def normalize_url(url):
-#     normalize = url[:7]
-#     # if normalize.replace('', 'https://'):
-#     # if normalize.replace('', 'https://') == normalize.replace('', ''):
-#     if normalize.replace('', 'https://') == normalize.replace('', '333'):
-#         print(normalize.replace('', 'https://', 1))
-#
-#     elif normalize.replace('http://', 'https://'):
-#         # print(normalize.replace('http://', 'https://'))
-#         print(normalize.replace('http://', '111'))
-#     # else:
-#     #     print(normalize.replace('https://', 'https://'))
-#     return "https://" + normalize
-# # s = 'hello'
-# # s1 = 'http://hello'
-# # s2 = 'https://hello'
-# # print(s.replace('', 'https://', 1) [:]) #работает
-# # print(s1.replace('http://', 'https://') [:])   #работает
-# # print(s2.replace('https://', 'https://') [:]) #работает
-#
-#
-# print(normalize_url('hello'))
-# print(normalize_url('http://hello1'))
-# # print(normalize_url('https://hello2'))
+def my_substr(string, current_char):
 
-# def normalize_url(url):
-#     normalize = url[:-1]
-#     if normalize == 'h':
-#         # return '222'
-#         return '444'
-#     return '33'
-#
-# print(normalize_url('https://sdefw'))
-# print(normalize_url('sdefw'))
-
-# def get_type_of_sentence(sentence):
-#
-#   last_char = sentence[0]
-#   if last_char == 'd':
-#       return 'perviy'
-#   elif last_char == '!':
-#       return 'vtoroy'
-#   return 'tretiy'
+    i = 0
+    reversed_string = '' #  инициализируем строку, куда будем записывать результат
+    current_char = string[i]    #   берем из строки символ по текущему индексу
+    reversed_string = reversed_string + current_char #  записываем в строку-результат новое значение: текущая строка-результат + новый символ
 
 
-# print(get_type_of_sentence('dorn'))
-# print(get_type_of_sentence('!http://Hodor?'))
+    while i <= len(string): #   условие: повторяем тело цикла, пока текущий индекс не дошел — до последнего символа
+        i = i + 1   #   обновляем счетчик
 
-# def normalize_url(url):
-#     if 'https://' in url:
-#         return url
-#     elif 'http://' in url:
-#         return 'https://' + url[7:]
-#     else:
-#         return 'https://' + url
-
-def normalize_url(url):
-    # if 'https://' in url:
-    #     return url
-    # elif url[0:3] != 'http':
-    #     return 'https://' + url
-    # elif 'http://' in url:
-    #     return 'https://' + url[7:]
-
-    # url = url[0:]
-    if url[:7] != 'http://' and 'https://':
-        return 'https://' + url
-    return url if 'https://' in url else ('https://' + url[7:])
+    return reversed_string  #   когда цикл завершился, возвращаем строку-результат
 
 
 
-print(normalize_url('yandex.ru'))
-print(normalize_url('http://yandex.ru'))
-print(normalize_url('https://yandex.ru'))
+
+string = 'If I look back I am lost'
+
+print(my_substr(string, 2))  # => 'I'
+# print(my_substr(string, 7))  # => 'If I lo'
+
+
 name = input('это конец')
-# если в первых 7 символах есть url то оставляем url, если url нет то добавляем его
