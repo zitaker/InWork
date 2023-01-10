05c0b43d9cb80efff2ad345b9542c2096768aed0
MyWork <92075508+zitaker@users.noreply.github.com>
2023-01-10 06:49:08 +0300

    Add files via upload

--- earlier/proba.py
+++ earlier/proba.py
@@ -1,28 +1,10 @@
-# from symbols import is_vowel
-#
-#
-# def count_vowels(text):
-#     result = 0
-#     for char in text:
-#         if is_vowel(char):
-#             result += 1
-#     return result
+from kit import *
 
-from symbols import is_vowel
+def print_kit():
+    print('qwe')
 
-def  count_vowels(words):
-    words_1 = 0
-    for words_2 in words:
-        if is_vowel(words_2):
-            words_1 += 1
-    return words_1
-
-print(is_vowel('a'))      # True
-print(is_vowel('n'))      # False)
-
-print(count_vowels('One'))  # 2
-print(count_vowels('London is the capital of Great Britain'))  # 13
-
+print('re')
+print_kit()
 
 name = input('это конец!!!')
 
